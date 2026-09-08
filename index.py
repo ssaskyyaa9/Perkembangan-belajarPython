0ae3653ea4c591b00b952036824d484561c6813b
ssaskyyaa9 <saskyyaa90@gmail.com>
2026-09-08 18:20:58 +0700

    first commit

---
+++ index.py
@@ -0,0 +1,29 @@
+# username = input("Masukkan username:")
+# print("Username anda :" + username)
+# print(f"Username anda : {username}")
+
+# -------- latihan 2 ---------
+# x = 4
+# y = 3
+# z = 2
+
+# w = (x + y * z) / (x * y) ** z
+# print(f"hasilnya adalah: {w}")
+
+# -------- latihan 3 ---------
+# angka = int(input("Silahkan masukan angka berapapun: "))
+# hasil = angka **2
+# print(f"Kuadrat dari {angka} adalah: {hasil}")
+
+# -------- latihan 4 ---------
+jumlah= 49
+rasio= 0.4
+
+usia_budi= jumlah / (1 + rasio)
+usia_andi= jumlah - usia_budi
+
+usiaBudi= int(usia_budi + 2)
+usiaAndi= int(usia_andi + 2)
+
+print(f"usia andi sekarang: {usiaAndi}")
+print(f"usia budi sekarang: {usiaBudi}")
